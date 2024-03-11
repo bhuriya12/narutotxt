b2bd2aac3697762ce12cd629275aa12466557597
bhuriya12 <162349127+bhuriya12@users.noreply.github.com>
2024-03-12 00:16:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ API_ID = API_ID = 26451206
 
 API_HASH = os.environ.get("API_HASH", "32984406271d6f3945bb536671b143a7")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7195520207:AAGp2n6zr6Bp2MAospRUKseIhykz0zSdtJ8")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6349863226:AAH8pv6lXiARBJeHIXbrS9ByRYRuJihjbrc")
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 OWNER = int(os.environ.get("OWNER", 6830450483))
 
-LOG = -1002120779186
+LOG = -1002067348939
