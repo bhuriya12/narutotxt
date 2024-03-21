b0ae06e94a44d13e208da1baf4ac16e23bf737ff
bhuriya12 <162349127+bhuriya12@users.noreply.github.com>
2024-03-22 01:02:35 +0530

    Update p_bar.py

--- p_bar.py
+++ p_bar.py
@@ -66,3 +66,31 @@ def hrt(seconds, precision = 0):
 
 timer = Timer()
 
+async def progress_bar(current, total, reply, start):
+    if timer.can_send():
+        now = time.time()
+        diff = now - start
+        if diff < 1:
+            return
+        else:
+            perc = f"{current * 100 / total:.1f}%"
+            elapsed_time = round(diff)
+            speed = current / elapsed_time
+            remaining_bytes = total - current
+            if speed > 0:
+                eta_seconds = remaining_bytes / speed
+                eta = hrt(eta_seconds, precision=1)
+            else:
+                eta = "-"
+            sp = str(hrb(speed)) + "/s"
+            tot = hrb(total)
+            cur = hrb(current)
+            
+            #don't even change anything till here
+            # Calculate progress bar dots
+            #ab mila dil ko sukun #by Kshitij
+            #change from here if you want 
+            bar_length = 11
+            completed_length = int(current * bar_length / total)
+            remaining_length = bar_length - completed_length
+            progress_bar = "▰" * completed_length + "▱" * remaining_length
